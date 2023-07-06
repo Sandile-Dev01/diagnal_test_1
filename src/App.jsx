ed7d12246a7655cab3d634c02ff574568215d6c6
sandile ndimande <ndimandesandile778@gmail.com>
2023-07-06 11:25:53 +0200

    fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import Content from "./pages/Content";
+import "./app.css";
 
 const App = () => {
   return (
     <>
       <Navbar />
+
+      <div className="movie_content">
+        <Content />
+        <Content />
+        <Content />
+      </div>
     </>
   );
 };
--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Img from "./assets/poster2.jpg";
 import "./content.css";
 
-const Content = () => {
-  return <div>Content</div>;
-};
+function Content() {
+  const [articleData, setArticleData] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = "https://test.create.diagnal.com/data/page1.json";
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data.response.results);
+        // Assuming the response data is in the structure you provided
+        const results = data.page["content-items"].content;
+        console.log(results);
+
+        const articleData = results.map((article) => article);
+        setArticleData(articleData);
+        console.log(articleData);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {articleData &&
+        articleData.map((article, index) => (
+          <div className="movie_item" key={index}>
+            <div className="article_category_container">
+              <div className="movie_img">
+                <img src={Img} alt="movie-poster" />
+              </div>
+
+              <h1>{article.name}</h1>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
 
 export default Content;
